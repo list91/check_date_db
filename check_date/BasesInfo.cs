d728baed1d7120ba4612489ae4909a13b63564ae
list91 <s_anufriev@list.ru>
2023-11-07 23:33:01 +0700

    Добавьте файлы проекта.

---
+++ check_date/BasesInfo.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace check_date
+{
+    public class BasesInfo
+    {
+        public string date { get; set; }
+        public string path { get; set; }
+        public BasesInfo(string date, string path)
+        {
+            this.date = date;
+            this.path = path;
+        }
+        public static string CheckDate(string inputDate)
+        {
+            DateTime currentDate = DateTime.Now.Date;
+            DateTime date;
+
+            if (DateTime.TryParseExact(inputDate, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+            {
+                if (date < currentDate)
+                {
+                    return "дата прошла";
+                }
+                else if (date > currentDate)
+                {
+                    return "дата не прошла";
+                }
+                else
+                {
+                    return "сегодняшняя дата";
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static List<BasesInfo> getBasesInfoList(string path)
+        {
+            List<BasesInfo> listBase = new List<BasesInfo>();
+            if (!File.Exists(path)) // проверка наличия файла списка
+            {
+                return null;
+            }
+
+            StreamReader sr_BasesList = new StreamReader(path); // чтение файла списка
+            string namebase, pathbase, datedel = "";
+
+            // парсинг файла списка
+            Dictionary<string, List<string>> basesData = new Dictionary<string, List<string>>();
+            string baseName = "";
+            List<string> strings = new List<string>();
+
+            while (!sr_BasesList.EndOfStream)
+            {
+                //Dictionary<string, List<string>> t = basesData;
+                string str = sr_BasesList.ReadLine();
+                // заполнение списка
+                if (str.Length != 0)
+                {
+                    if (str[0] == '[') // если строка начинается с [, то в ней содержится название базы
+                    {
+                        str = str.Remove(0, 1);
+                        str = str.Remove(str.Length - 1, 1);
+                        baseName = str;
+                        strings = new List<string>();
+                    }
+                    else if (str != null && str != "" && baseName != "")
+                    {
+                        strings.Add(str);
+                        basesData[baseName] = strings;
+                    }
+                }
+            }
+            if (basesData.Count != 0)
+            {
+                foreach (string base_name in basesData.Keys)
+                {
+                    string name = base_name;
+                    string pathExe = "";
+                    string dateDel = "";
+
+                    List<string> lines = basesData[base_name];
+                    foreach (string base_line in lines)
+                    {
+                        if (base_line.Contains("DateDel="))
+                        {
+                            int index = base_line.IndexOf("DateDel=");
+                            string substr = base_line.Substring(index + 8);
+
+                            if (substr.Length >= 10 && Regex.IsMatch(substr, @"\d{2}\.\d{2}\.\d{4}"))
+                            {
+                                dateDel = substr;
+                                //if (!isDel) { isDel = true; }
+                            }
+                        } else if (base_line.Contains("Connect=File=")) {
+
+                            string input = base_line;//"Connect=File=\"C:\\Bases\\РТ Проф\";";
+                            string pattern = @"Connect=File=""([^""]+)"";";
+
+                            Match match = Regex.Match(input, pattern);
+                            if (match.Success)
+                            {
+                                pathExe = match.Groups[1].Value;
+                                // Дальнейшие действия с путем до папки
+                            }
+                        }
+                    }
+                    if (dateDel!="" && pathExe!="") 
+                    {
+                        listBase.Add(new BasesInfo(dateDel, pathExe));
+                    }
+                }
+            }
+            return listBase;
+        }
+    }
+}
---
+++ check_date/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+// See https://aka.ms/new-console-template for more information
+using check_date;
+
+var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+string basesFilePath = userFolder + $"\\AppData\\Roaming\\1C\\1CEStart\\ibases.v8i";
+List<BasesInfo> bases = BasesInfo.getBasesInfoList(basesFilePath);
+string logFilePath = Directory.GetCurrentDirectory() + "\\log.txt";
+
+foreach (BasesInfo baseItem in bases)
+{
+    string result = BasesInfo.CheckDate(baseItem.date);
+    if (result != null)
+    {
+        if (result == "дата прошла" || result == "сегодняшняя прошла")
+        {
+            if (File.Exists(logFilePath))
+            {
+                using (StreamWriter sw = File.AppendText(logFilePath))
+                {
+                    DateTime now = DateTime.Now;
+                    sw.WriteLine(now.Day + "." + now.Month + "." + now.Year + "." + now.Hour + "." + now.Minute + "." + now.Second + " удалить - " + baseItem.path);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(logFilePath))
+                {
+                    DateTime now = DateTime.Now;
+                    sw.WriteLine(now.Day + "." + now.Month + "." + now.Year + "." + now.Hour + "." + now.Minute + "." + now.Second + " удалить - " + baseItem.path);
+                }
+            }
+        }
+    }
+}
+Console.ReadKey();
